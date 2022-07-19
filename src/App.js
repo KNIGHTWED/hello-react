78b6bc8aa7e3ec88176f531e3511f22877775984
Jeongmin Kim <jminkim@tecace.com>
2022-07-19 16:43:44 +0900

    0719

--- src/App.js
+++ src/App.js
@@ -2,20 +2,19 @@ import React, {Component} from "react";
 import "./App.css";
 import MyComponent from "./MyComponent";
 import Counter from "./Counter";
+import Say from "./Say"
+import EventPracticeClass from "./EventPracticeClass";
+import EventPracticeFun from "./EventPracticeFun";
+import EventPracticeObj from "./EventPracticeObj";
+
+
 // const 와 let 사용
 // 정적     동적
 
 // 함수형 컴포넌트
-// function App() {
-//   const name = "React";
-//   return (
-//     <MyComponent/>
-//   );
-// }
-
 const App = () =>{
   // return <MyComponent name="react" favoriteNumber={3}>리액트</MyComponent>;
-  return <Counter/>;
+  return <EventPracticeObj />
 }
 
 // 클래스형 컴포넌트
--- src/Counter.js
+++ src/Counter.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  // 컴포넌트 생성자
-  constructor(props){
-    super(props);
-    // state의 초기값 설정
-    this.state = {
-      number: 0
+  state = {
+      number: 0,
+      fixedNumber: 0
     };
-  }
   render() {
-    const {number} = this.state;
+    const {number, fixedNumber} = this.state;
     return (
       <div>
         <h1>{number}</h1>
-        <button onClick={()=>{
-          this.setState({number: number + 1});
-        }}>
+        <h2>바뀌지 않는 값: {fixedNumber}</h2>
+        <button 
+        // 버튼 클릭하면 1씩 더해주는 함수
+        onClick={() => {
+          this.setState(
+            {
+              number: number + 1
+            },
+            () => {
+              console.log('called setState just before');
+              console.log(this.state);
+            }
+          );
+          }}>
           +1
         </button>
       </div>
